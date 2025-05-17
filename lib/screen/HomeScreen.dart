3817ad4b818387073b8027394ae9d043f79da64f
Aloamoh Alemanji <aloamoh@icloud.com>
2025-05-17 14:46:44 -0400

    initial commit

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
 
 class Homescreen extends StatefulWidget {
-  const  Homescreen({super.key});
+  const Homescreen({super.key});
 
   @override
-  State<Homescreen> createState() => _MyWidgetState();
+  State<Homescreen> createState() => _HomescreenState();
 }
 
-class _MyWidgetState extends State<Homescreen> {
+class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold
-    (body; Container(
-      decoration: BoxDecoration
-         image; DecorationImage
-         image; AssetImage('assets/background.png'),
-         fit; BoxFit.fill,
-
-      ), // Conatainer
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
     );
+  }
+}
+
+class TopBar extends StatefulWidget implements PreferredSizeWidget {
+  const TopBar({super.key});
+
+  @override
+  State<TopBar> createState() => _TopBarState();
+
+  @override
+  Size get preferredSize => const Size(double.maxFinite, 60);
+}
+
+class _TopBarState extends State<TopBar> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      child: Material(
+        color: Color.fromARGB(255, 26, 26, 26),
+        child: Container(
+          color: Color.fromARGB(255, 26, 26, 26),
+          child: Row(
+            children: [ClipRect(BorderRadius: BorderRadius.circular(40))],);
   }
 }
